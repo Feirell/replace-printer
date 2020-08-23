bbe9fbefc3475fcf85e50915e9eac01a57df70c8
Florian Völker <Neirolf2030@web.de>
2020-08-23 23:37:08 +0200

    extended replace printer

--- src/replace-printer.ts
+++ src/replace-printer.ts
@@ -4,7 +4,7 @@ import {WriteStreamStringBuffer} from "./write-stream-string-buffer";
 import TTY from "tty";
 
 interface ReplacePrinterOptions {
-    outStream?: WriteStream;
+    outStream?: TTYWriteStream;
 }
 
 function removeProblematicCharacters(str: string) {
@@ -13,58 +13,59 @@ function removeProblematicCharacters(str: string) {
     return str.replace(/\t/g, '    ');
 }
 
+function getLineCountAndLastLineLength(msg: string, columns?: number) {
+    let lineCount = 0;
+    let lastLineLength = 0;
+
+    for (const char of msg) {
+
+        // using lastLineLength == columns since the terminal will
+        // linebreak even if it is not suggested by a \n
+        if (char == '\n') {
+            lineCount++;
+            lastLineLength = 0;
+        } else if (lastLineLength === columns) {
+            lineCount++;
+            lastLineLength = 1;
+        } else {
+            lastLineLength += 1;
+        }
+    }
+
+    return {lineCount, lastLineLength};
+}
+
 type TTYWriteStream = TTY.WriteStream & { moveCursor: (dx: number, dy: number) => undefined };
+
+export const isTTYStream = (ws: any): ws is TTYWriteStream => !!(ws as TTYWriteStream).isTTY
 
 export class ReplacePrinter {
     public readonly replaceConsole: Console;
     public readonly continuesConsole: Console;
 
-    private outStream: TTYWriteStream;
+    private readonly outStream: TTYWriteStream;
 
-    private replaceStreamBuffer: WriteStreamStringBuffer;
-    private continuesStreamBuffer: WriteStreamStringBuffer;
+    private readonly replaceStreamBuffer: WriteStreamStringBuffer;
+    private readonly continuesStreamBuffer: WriteStreamStringBuffer;
 
     private lastReplaceMessage?: string;
 
-    constructor(options: ReplacePrinterOptions = {}) {
-        const {
-            outStream = process.stdout
-        } = options;
+    constructor({
+                    outStream = process.stdout as TTYWriteStream
+                }: ReplacePrinterOptions = {}) {
 
-        if (!(outStream as TTYWriteStream).isTTY)
-            throw new Error('outstream needs to be a TTY stream');
+        if (!isTTYStream(outStream))
+            throw new Error('outStream needs to be a TTY stream');
 
-
-        this.outStream = outStream as TTYWriteStream;
+        this.outStream = outStream;
 
         const notice = this.pushBuffers.bind(this);
 
         this.replaceStreamBuffer = new WriteStreamStringBuffer(this.outStream, notice, 'replace');
-        this.continuesStreamBuffer = new WriteStreamStringBuffer(this.outStream, notice);
-
         this.replaceConsole = new Console(this.replaceStreamBuffer);
+
+        this.continuesStreamBuffer = new WriteStreamStringBuffer(this.outStream, notice, 'append');
         this.continuesConsole = new Console(this.continuesStreamBuffer);
-    }
-
-    private getLineAndLastColumns(msg: string, columns?: number) {
-        const columnsKnown = columns === undefined;
-
-        let lineCount = 0;
-        let lastLineLength = 0;
-
-        for (const char of msg) {
-
-            // using lastLineLength == columns since the terminal will
-            // linebreak even if it is not suggested by a \n
-            if (char == '\n' || lastLineLength == columns) {
-                lineCount++;
-                lastLineLength = lastLineLength == columns ? 1 : 0;
-            } else {
-                lastLineLength += 1;
-            }
-        }
-
-        return {lineCount, lastLineLength};
     }
 
     private pushBuffers() {
@@ -76,15 +77,20 @@ export class ReplacePrinter {
             return;
         }
 
-        const replaceBuffer = removeProblematicCharacters(this.replaceStreamBuffer.buffer.slice(0, -1)) || this.lastReplaceMessage; // removing the last linebreak
+        // since the buffer is written by a Console there is always a linebreak at the end
+        // removing it to have it sit flush on the last line
+        const clearedMessage = removeProblematicCharacters(this.replaceStreamBuffer.buffer.slice(0, -1));
+        const replaceBuffer = clearedMessage.length == 0 ? this.lastReplaceMessage : clearedMessage;
+        // clear buffer to remember that this message was already printed and is now cleaned in the lastReplaceMessage
         this.replaceStreamBuffer.buffer = '';
 
         const continuesBuffer = this.continuesStreamBuffer.buffer;
+        // clear the buffer to not reprint the message since it is now already in the output buffer
         this.continuesStreamBuffer.buffer = '';
 
+        // clear the space the last replace message took
         if (this.lastReplaceMessage != undefined) {
-            // can't make really
-            const measurements = this.getLineAndLastColumns(this.lastReplaceMessage, os.columns);
+            const measurements = getLineCountAndLastLineLength(this.lastReplaceMessage, os.columns);
 
             // clearing last printed replaceable part
             os.moveCursor(-measurements.lastLineLength, -measurements.lineCount);
@@ -94,8 +100,6 @@ export class ReplacePrinter {
         this.lastReplaceMessage = replaceBuffer;
 
         // printing static and replaceable console output
-        const assembledMessage = continuesBuffer + replaceBuffer;
-
-        os.write(assembledMessage);
+        os.write(continuesBuffer + replaceBuffer);
     }
 }
